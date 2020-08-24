de558a6545bd041230add87f4f82f0557e253dc0
Nickolay Mikhalko <mikhalko98@gmail.com>
2020-08-24 23:26:59 +0300

    Initial dist subtree commit

---
+++ dist/precache-manifest.a2e290bd872dd2d50b344a6de8c3d94f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da43db583ba17a3eee6e",
+    "url": "first-pwa/css/app.fb230f35.css"
+  },
+  {
+    "revision": "b7ae5d6edac891c76566",
+    "url": "first-pwa/css/chunk-vendors.036b6237.css"
+  },
+  {
+    "revision": "33f5d213687dde6c9983",
+    "url": "first-pwa/css/details.c278434f.css"
+  },
+  {
+    "revision": "68b9c7eddf837ef48954",
+    "url": "first-pwa/css/search.9e1be7cb.css"
+  },
+  {
+    "revision": "a9b8ec3a8f8c5af3ccb96f271a66b4fc",
+    "url": "first-pwa/index.html"
+  },
+  {
+    "revision": "da43db583ba17a3eee6e",
+    "url": "first-pwa/js/app.b1df2779.js"
+  },
+  {
+    "revision": "b7ae5d6edac891c76566",
+    "url": "first-pwa/js/chunk-vendors.5c12b668.js"
+  },
+  {
+    "revision": "33f5d213687dde6c9983",
+    "url": "first-pwa/js/details.f5fba6a7.js"
+  },
+  {
+    "revision": "73cf230f15f7f1c34a4e",
+    "url": "first-pwa/js/details~search.9839bc07.js"
+  },
+  {
+    "revision": "68b9c7eddf837ef48954",
+    "url": "first-pwa/js/search.d7ff009f.js"
+  },
+  {
+    "revision": "ecc2e9a2075f6a1b4c7be625498ee3c9",
+    "url": "first-pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "first-pwa/robots.txt"
+  }
+]);
